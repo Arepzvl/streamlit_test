aff3ba925a56c420070b2598f963a8ca7e267801
Arep <powerakmalpower@gmail.com>
2025-05-29 15:24:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # Set the app title 
-st.title('This Is My First HAKIM SIAL!!') 
+st.title('This Is My First Streamlit!!') 
 
 # Add a welcome message 
 st.write('Welcome to my Streamlit app!') 
